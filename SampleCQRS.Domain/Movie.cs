aebbd13ee92232824c08f114042e30471c6d321b
exceptionnotfound <mpjones428@gmail.com>
2015-12-28 09:27:03 -0700

    Added domain objects.

---
+++ SampleCQRS.Domain/Movie.cs
@@ -0,0 +1,17 @@
+﻿using SimpleCqrs.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCQRS.Domain
+{
+    public class Movie : AggregateRoot
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int RunningTimeMinutes { get; set; }
+        public List<MovieReview> Reviews { get; set; }
+    }
+}
---
+++ SampleCQRS.Domain/MovieReview.cs
@@ -0,0 +1,17 @@
+﻿using SimpleCqrs.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCQRS.Domain
+{
+    public class MovieReview : AggregateRoot
+    {
+        public string Content { get; set; }
+        public string Reviewer { get; set; }
+        public string Publication { get; set; }
+
+    }
+}
